43452fbeec93dd2725d59c713fb9ffe51ef2c40b
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-17 11:31:22 -0400

    description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,10 +65,11 @@ export default function Home () {
             sizes='100vw'
           />
         </div>
-        <div>
+        <div className='description'>
           <h2 className='nebula'>
             Nebula Cosmic
           </h2>
+          <p style={{color:"#3D3D3D",fontWeight:'100'}}>Quando iniciamos a Bikcraft queriamos apenas <br/> um produto que adoraríamos utilizar. Eramos <br/> apaixonados por pedalar e também criar.</p>
         </div>
       </main>
 
